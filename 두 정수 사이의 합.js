70a12329e7ce498b9ea15888445d649c2fd89a68
donghoonlee94 <94donghoon.lee@gmail.com>
2020-06-03 22:11:33 +0900

    [reat] Test code Refactoring

--- 두 정수 사이의 합.js
+++ 두 정수 사이의 합.js
@@ -1,17 +1,15 @@
-export const solution = (a, b) => {
-  let maxNum;
-  let minNum;
-  let array = [];
-  if (a > b) {
-    maxNum = a;
-    minNum = b;
-  }
-  if (b > a) {
-    maxNum = b;
-    minNum = a;
-  }
+const arrayCompare = (a, b) => {
+  const array = [];
+  const maxNum = a > b ? a : b;
+  const minNum = a < b ? a : b;
+
   for (let i = minNum; i <= maxNum; i++) {
     array.push(i);
   }
-  return array.reduce((ac, cu) => ac + cu);
+
+  return array;
+};
+
+export const solution = (a, b) => {
+  return a === b ? a : arrayCompare(a, b).reduce((ac, cu) => ac + cu);
 };
