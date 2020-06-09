69169f6eb1090b87b81a6721232d01302ae249bd
donghoonlee94 <94donghoon.lee@gmail.com>
2020-06-09 23:17:18 +0900

    [refactoring] 평균구하기 리팩토링 괴짜버전

--- 평균구하기.js
+++ 평균구하기.js
@@ -1,3 +1,20 @@
+// 배열의 수를 모두 합한 뒤 합한 수의 평균을 구하는 코드.
+
+// export const solution = (arr) => {
+//   return arr.reduce((ac, cu) => ac + cu) / arr.length;
+// }
+
+
+export const arrTotal = (arr) => {
+  return arr.reduce((ac, cu) => ac + cu);
+}
+
 export const solution = (arr) => {
-  return arr.reduce((ac, cu) => ac + cu) / arr.length;
+  const sum = arrTotal(arr);
+  const obj = {
+    [sum]: (len) => {
+      return sum / len;
+    }
+  }
+  return obj[sum](arr.length);
 }
