ec3f900c9be22f5ce91e6ab348e05469e433851f
donghoonlee94 <94donghoon.lee@gmail.com>
2020-06-29 23:15:26 +0900

    [refactoring] 완주하지 못한 선수 리팩토링

--- 완주하지 못한 선수.js
+++ 완주하지 못한 선수.js
@@ -1,7 +1,10 @@
-export function solution(participant, completion) {
-  for (let i = 0; i <= participant.length; i++) {
-    if (!completion.includes(participant[i])) {
-      return participant[i];
-    }
+
+export function compare(participant, completion) {
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) return participant[i]
   }
+}
+
+export function solution(participant, completion) {
+  return compare(participant.sort(), completion.sort());
 }
--- 완주하지 못한 선수.test.js
+++ 완주하지 못한 선수.test.js
@@ -1,17 +1,31 @@
 import { solution } from "./완주하지 못한 선수";
 
-const participantArray1 = ['leo', 'kiki', 'eden'];
-const participantArray2 = ['marina', 'josipa', 'nikola', 'vinko', 'filipa'];
-const participantArray3 = ['mislav', 'stanko', 'mislav', 'ana'];
 
-const completionArray1 = ['kiki', 'eden'];
-const completionArray2 = ['marina', 'josipa', 'nikola', 'filipa'];
-const completionArray3 = ['mislav', 'stanko', 'ana'];
 
 describe("완주하지 못한 선수 테스트", () => {
-  it("완주하지 못한 선수", () => {
-    expect(solution(participantArray1, completionArray1)).toEqual("leo");
-    expect(solution(participantArray2, completionArray2)).toEqual("vinko");
-    expect(solution(participantArray3, completionArray3)).toEqual("mislav");
+  it("test1", () => {
+    const participantArr = ['leo', 'kiki', 'eden'];
+    const completionArr = ['kiki', 'eden'];
+    expect(solution(participantArr, completionArr)).toEqual("leo");
+  });
+  it("test2", () => {
+    const participantArr = ['marina', 'josipa', 'nikola', 'vinko', 'filipa'];
+    const completionArr = ['marina', 'josipa', 'nikola', 'filipa'];
+    expect(solution(participantArr, completionArr)).toEqual("vinko");
+  });
+  it("test3", () => {
+    const participantArr = ['mislav', 'stanko', 'mislav', 'ana'];
+    const completionArr = ['mislav', 'stanko', 'ana'];
+    expect(solution(participantArr, completionArr)).toEqual("mislav");
+  });
+  it("test4", () => {
+    const participantArr = ['mislav', 'stanko', 'mislav', 'stanko'];
+    const completionArr = ['mislav', 'stanko', 'mislav'];
+    expect(solution(participantArr, completionArr)).toEqual("stanko");
+  });
+  it("test5", () => {
+    const participantArr = ['js', 'yarn', 'stanko', 'yarn', 'dragon', 'dev', 'local', 'mislav', 'js', 'java', 'yarn', 'python', 'stanko', 'php', 'mislav'];
+    const completionArr = ['mislav', 'yarn', 'mislav', 'stanko', 'dev', 'js', 'java', 'python', 'php', 'dragon', 'yarn', 'stanko', 'js', 'yarn'];
+    expect(solution(participantArr, completionArr)).toEqual("local");
   });
 });
